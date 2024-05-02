73dfaa1f7d4fd78dd82324dfd9e1c8c30ef7a8b3
Elliot <ellthrowaway2@icloud.com>
2024-05-02 15:14:11 -0400

    cal counter input validation using regex

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const calorieCounter = document.getElementById('calorie-counter');
-const budgetNumberInput = document.getElementById('budget');
-const entryDropdown = document.getElementById('entry-dropdown');
-const addEntryButton = document.getElementById('add-entry');
-const clearButton = document.getElementById('clear');
-const output = document.getElementById('output');
-let isError = false;
+const calorieCounter = document.getElementById('calorie-counter')
+const budgetNumberInput = document.getElementById('budget')
+const entryDropdown = document.getElementById('entry-dropdown')
+const addEntryButton = document.getElementById('add-entry')
+const clearButton = document.getElementById('clear')
+const output = document.getElementById('output')
+let isError = false
 
 const cleanInputString = (str) => {
-    const regex = /\+-/
+    const regex = /[+-\s]/g
+    return str.replace(regex, '')
+}
+
+const isInvalidInput = (str) => {
+    const regex = /\d+e\d+/i
+    return str.match(regex)
 }
